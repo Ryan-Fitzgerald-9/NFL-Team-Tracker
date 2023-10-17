668da7d8b30799f2a0bfe3f1c8d7472a01ec7f94
Ryan-Fitzgerald-9 <rfitzgerald806@gmail.com>
2023-10-17 10:55:02 -0400

    schedule and home buttons

--- client/script.js
+++ client/script.js
@@ -16,6 +16,10 @@ const base = 'http://localhost:3001/'
 
 
 // FUNCTIONS
+const homeBtnClick = () => {
+    infoBtns.replaceChildren()
+    mainBody.replaceChildren()
+}
 
 const rosterBtnClick = async () => {
     infoBtns.replaceChildren()
@@ -45,8 +49,39 @@ const rosterBtnClick = async () => {
     }
   }
 
+  const scheduleBtnClick = async () => {
+    infoBtns.replaceChildren()
+    mainBody.replaceChildren()
+  
+    try {
+      const scheduleResponse = await axios.get(`${base}schedule`)
+      const scheduleData = scheduleResponse.data[0].games
+      console.log(scheduleData)
+  
+  
+      if (scheduleData.length > 0) {
+        scheduleData.forEach((games) => {
+          const scheduleText = `
+            <h5 class="week">${games.week}</h5>
+            <p class="date">${games.date}</p>
+            <p class="opponent">${games.opponent}</p>
+            <p class="location">${games.location}</p>
+            <p class="result">${games.result}</p>
+            <p class="score">${games.score}</p>`
+  
+          mainBody.innerHTML += scheduleText;
+        })
+      } else {
+        mainBody.innerHTML = `<div class="not-found">Game not found</div>`;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 
 // EVENT LISTENERS
 
 rosterBtn.addEventListener('click', rosterBtnClick)
+scheduleBtn.addEventListener('click', scheduleBtnClick)
+homeBtn.addEventListener('click', homeBtnClick)
