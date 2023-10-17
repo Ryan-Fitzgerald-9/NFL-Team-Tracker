9ec4558bbd0a85d146ec18edcd2f6cb6e995be86
Ryan-Fitzgerald-9 <rfitzgerald806@gmail.com>
2023-10-17 09:16:49 -0400

    dom elements added

---
+++ client/script.js
@@ -0,0 +1,24 @@
+// DOM ELEMENTS
+const searchBar = document.querySelector('.search-bar')
+const searchBtn = document.querySelector('.search-button')
+const infoBtns = document.querySelector('.info-button')
+const mainBody = document.querySelector('.main-body')
+const homeBtn = document.querySelector('#home')
+const rosterBtn = document.querySelector('#roster')
+const depthChartBtn = document.querySelector('#depth-chart')
+const scheduleBtn = document.querySelector('#schedule')
+
+
+// VARIABLES
+
+
+
+
+
+// FUNCTIONS
+
+
+
+
+
+// EVENT LISTENERS
