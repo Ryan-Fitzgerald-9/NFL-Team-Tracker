a0567543d95de81a93bd44f3aebc99a0009d5b4b
Ryan-Fitzgerald-9 <rfitzgerald806@gmail.com>
2023-10-17 10:13:23 -0400

    roster button js populates players

--- client/script.js
+++ client/script.js
@@ -10,15 +10,43 @@ const scheduleBtn = document.querySelector('#schedule')
 
 
 // VARIABLES
-
+const base = 'http://localhost:3001/'
 
 
 
 
 // FUNCTIONS
 
-
+const rosterBtnClick = async () => {
+    infoBtns.replaceChildren()
+    mainBody.replaceChildren()
+  
+    try {
+      const playersResponse = await axios.get(`${base}player`)
+      const playersData = playersResponse.data
+      console.log(playersData)
+  
+  
+      if (playersData.length > 0) {
+        playersData.forEach((player) => {
+          const playerText = `<div class="item-holder">
+            <img src="${player.image}" alt="" class="player-pic">
+            <h3 class="player-name">${player.name}</h3>
+            <p class="position">${player.position} #${player.number}</p>
+            <p class="college">College: ${player.college}</p>`
+  
+          mainBody.innerHTML += playerText;
+        })
+      } else {
+        mainBody.innerHTML = `<div class="not-found">Players not found</div>`;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 
 
 // EVENT LISTENERS
+
+rosterBtn.addEventListener('click', rosterBtnClick)
